18df661f272268918694b8fb90bef5a452894aca
MrKo1H <astanevercry@gmail.com>
2022-01-02 17:22:52 +0700

    9.asm

---
+++ week3/9.asm
@@ -0,0 +1,26 @@
+section .data:
+	format1 db '%d', 0	
+	nhapn db 'nhap n=',0
+	format2 db '%d', 0
+
+section .text:
+	global start
+	extern _scanf
+	extern _printf
+start:
+	xor rbp, rbp
+		
+	; printf('nhap n=');
+	push format1	
+	call _printf
+	add rsp, 0x8
+
+	; scanf('%d');
+	push format2	  
+	call _scanf
+
+	call exit
+	
+exit:
+	mov rax, 1
+	syscall		
